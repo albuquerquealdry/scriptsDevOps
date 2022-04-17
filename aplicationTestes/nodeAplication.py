d35b06356bcd1e08b417ef4db82938b93c875f6b
albuquerquealdry <albuquerquealdry@gmail.com>
2022-04-17 14:34:14 -0300

    devops

--- aplicationTestes/nodeAplication.py
+++ aplicationTestes/nodeAplication.py
@@ -1,29 +1,27 @@
 import os
 import time
 import requests
+import dotenv
 
-HOST = "localhost"
-METHOD = "http://"
-ENDPOINT = "receita"
-
-PORT = "3000"
-IMAGE = "despesa"
-TAGAPLICATION = "despesa"
-PROVIDER = "aldry1303"
-TAGVERSION = "1.0.2"
 
 os.system("cd .. & git clone https://github.com/albuquerquealdry/vagrant-provision.git ")
 os.system(f"cd .. & cd vagrant-provision/dev-environments/aplication-node/nodeAplication && sed -i 's/IMAGE/'{IMAGE}'/g' Vagrantfile && sed -i 's/PORT/'{PORT}'/g' Vagrantfile && sed -i 's/PROVIDER/'{PROVIDER}'/g' Vagrantfile && sed -i 's/TAGVERSION/'{TAGVERSION}'/g' Vagrantfile && sed -i 's/TAGAPLICATION/'{TAGAPLICATION}'/g' Vagrantfile " )
 os.system("cd .. & cd vagrant-provision/dev-environments/aplication-node/nodeAplication  && vagrant up --provision")
-os.system ("cd .. & rm -rf vagrant-provision")
 
+time.sleep(3)
 url = f"{METHOD}{HOST}:{PORT}/{ENDPOINT}"
-teste = requests.get(url).status_code
-print(teste)
-if teste == 200 or 201:
+request = requests.get(url).status_code
+if request == 200 or 201:
+    n = 0
     print ("TESTING APLICATION")
-    time.sleep(120)
+    while n != 10:
+        n = n+1
+        request = requests.get(url).status_code
+        print(request)
     os.system ("cd .. & cd vagrant-provision/dev-environments/aplication-node/nodeAplication && vagrant destroy -f ")
+    os.system ("cd .. & rm -rf vagrant-provision")
     
 else:
     print ("Application NO OK")
+    os.system ("cd .. & cd vagrant-provision/dev-environments/aplication-node/nodeAplication && vagrant destroy -f ")
+    os.system ("cd .. & rm -rf vagrant-provision")
